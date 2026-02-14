10ad289ad6f5de4a52cbc15e7e902874e3db13c5
Deiver Julio <deiver-jc@live.com>
2026-02-14 15:18:48 -0500

    build(api): install and setup laravel restify with sanctum

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('login', \App\Http\Controllers\Restify\Auth\LoginController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.login');
+Route::post('register', \App\Http\Controllers\Restify\Auth\RegisterController::class)
+    ->name('restify.register');
+Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.forgotPassword');
+Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.forgotPassword');
+Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.forgotPassword');
+Route::post('resetPassword', \App\Http\Controllers\Restify\Auth\ResetPasswordController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.resetPassword');
+Route::post('verify/{id}/{hash}', \App\Http\Controllers\Restify\Auth\VerifyController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.verify');
+
+Route::post('verify/{id}/{hash}', \App\Http\Controllers\Restify\Auth\VerifyController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.verify');
+
